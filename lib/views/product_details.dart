769326f25da1ff74c0e619965aa9a2b830ccd755
Bikash Gurung <bikashgurung.official@gmail.com>
2024-10-25 17:30:14 +0545

    imageslide issue is now solved

--- lib/views/product_details.dart
+++ lib/views/product_details.dart
@@ -13,42 +13,28 @@ class ProductDetailScreen extends StatefulWidget {
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    // Wait for the first frame to complete
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      // Access child elements here safely
-      checkInternet();
       addImage();
     });
   }
 
-  checkInternet() async {
-    appCtrls.globalCtrl.internetConnection.isTrue
-        ? Get.snackbar(
-            'Internet Connected',
-            'Internet connection Successful.',
-            snackPosition: SnackPosition.BOTTOM,
-          )
-        : Get.snackbar(
-            'No Internet',
-            'Please check your internet connection.',
-            snackPosition: SnackPosition.BOTTOM,
-          );
-  }
-
   addImage() {
-    appCtrls.productsController.imageList.add(appCtrls
-        .productsController.productDetail.value.data!.image!.first.path
-        .toString());
+    appCtrls.productsController.imageList.add(
+      appCtrls.productsController.productDetail.value.data!.image!.first.path
+          .toString(),
+    );
     for (int i = 0;
         i <
             appCtrls.productsController.productDetail.value.data!
                 .variantDetails!.length;
         i++) {
-      appCtrls.productsController.imageList.add(appCtrls.productsController
-          .productDetail.value.data!.variantDetails![i].image!.first.path
-          .toString());
+      appCtrls.productsController.imageList.add(
+        appCtrls.productsController.productDetail.value.data!.variantDetails![i]
+            .image!.first.path
+            .toString(),
+      );
     }
   }
 
@@ -62,42 +48,37 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
           IconButton(onPressed: () {}, icon: const Icon(Icons.trolley))
         ],
       ),
-      body: Column(
-        children: [
-          ImageSlideshow(
-            /// Width of the [ImageSlideshow].
-            width: double.infinity,
+      body: Obx(() {
+        // Check if imageList has images before displaying the slideshow
+        if (appCtrls.productsController.imageList.isEmpty) {
+          return Center(
+              child:
+                  CircularProgressIndicator()); // Placeholder or loading indicator
+        }
 
-            /// Height of the [ImageSlideshow].
-            height: 200,
-
-            /// The page to show when first creating the [ImageSlideshow].
-            initialPage: 0,
-
-            /// The color to paint the indicator.
-            indicatorColor: Colors.blue,
-
-            /// The color to paint behind th indicator.
-            indicatorBackgroundColor: Colors.grey,
-
-            /// Auto scroll interval.
-            /// Do not auto scroll with null or 0.
-            autoPlayInterval: 3000,
-
-            /// Loops back to first slide.
-            isLoop: true,
-            children: [
-              for (int i = 0;
-                  i < appCtrls.productsController.imageList.length;
-                  i++)
-                Image.network(
-                  appCtrls.productsController.imageList[i],
-                  fit: BoxFit.cover,
-                ),
-            ],
-          )
-        ],
-      ),
+        return Column(
+          children: [
+            ImageSlideshow(
+              width: double.infinity,
+              height: 200,
+              initialPage: 0,
+              indicatorColor: Colors.blue,
+              indicatorBackgroundColor: Colors.grey,
+              autoPlayInterval: 3000,
+              isLoop: true,
+              children: [
+                for (int i = 0;
+                    i < appCtrls.productsController.imageList.length;
+                    i++)
+                  Image.network(
+                    appCtrls.productsController.imageList[i],
+                    fit: BoxFit.cover,
+                  ),
+              ],
+            ),
+          ],
+        );
+      }),
     );
   }
 }
